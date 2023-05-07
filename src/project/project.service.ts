ebea5bb96371db89c1caa8de308576b008497f20
Kamala864 <joshikamala12@gmail.com>
2023-05-07 13:53:11 +0545

    swited btanch

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LevenshteinDistance } from 'natural';
 
 @Injectable()
 export class ProjectService {
@@ -11,6 +12,50 @@ export class ProjectService {
   }
 
   findAll() {
+    const newProjectName = 'New Project Name';
+    const newProjectDescription = 'Description of the new project';
+
+    const existingProjects = [
+      {
+        name: 'Existing Project 1',
+        description: 'Description of existing project 1',
+      },
+      {
+        name: 'Existing Project 2',
+        description: 'Description of existing project 2',
+      },
+    ];
+
+    for (const project of existingProjects) {
+      const nameDistance = LevenshteinDistance(newProjectName, project.name);
+      const descriptionDistance = LevenshteinDistance(
+        newProjectDescription,
+        project.description,
+      );
+
+      // Normalize the distances to obtain similarity scores
+      const nameSimilarity =
+        (1 -
+          nameDistance / Math.max(newProjectName.length, project.name.length)) *
+        100;
+      const descriptionSimilarity =
+        (1 -
+          descriptionDistance /
+            Math.max(
+              newProjectDescription.length,
+              project.description.length,
+            )) *
+        100;
+
+      console.log(
+        `Name similarity with ${project.name}: ${nameSimilarity.toFixed(2)}%`,
+      );
+      console.log(
+        `Description similarity with ${
+          project.name
+        }: ${descriptionSimilarity.toFixed(2)}%`,
+      );
+    }
     return this.prisma.project.findMany();
   }
 
